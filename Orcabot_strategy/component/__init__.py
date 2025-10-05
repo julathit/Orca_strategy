46be7821e15e2d1c509762d1271d9efca6f461fd
julathit <newn8684@gmail.com>
2025-10-05 17:18:03 +0700

    add robot class and manual drive implementation

--- Orcabot_strategy/component/__init__.py
+++ Orcabot_strategy/component/__init__.py
@@ -1,1 +1,2 @@
-from component.robot import Robot
+from Orcabot_strategy.component.robot import Robot
+from Orcabot_strategy.component.manulDrive import manulDrive as  C_manulDrive
--- Orcabot_strategy/component/robot.py
+++ Orcabot_strategy/component/robot.py
@@ -4,12 +4,12 @@ from rclpy.node import Node
 from grsim_ros_bridge_msgs.msg import SSL
 
 class Robot:
-    def __init__(self, team:str, rid: int,msg: SSL):
+    def __init__(self,team:str, rid: int):
         self.id: int = rid
         self.role = None
         self.team = team
         
-        self.ssl_msg: SSL = msg
+        self.ssl_msg: SSL = SSL()
         
     def getRole(self):
         return self.role
@@ -29,11 +29,12 @@ class Robot:
     def __distance(self, origins: np.array , points: list) -> list:
         ...
             
-    def sendCommand(self, x: float, y: float, z: float, kickPower = False, dribbler = False):
+    def sendCommand(self,TestSSL: Node ,x: float, y: float, z: float, kickPower = False, dribbler = False):
         self.ssl_msg.cmd_vel.angular.z = z
         self.ssl_msg.cmd_vel.linear.x = x
         self.ssl_msg.cmd_vel.linear.y = y
         self.ssl_msg.kicker = kickPower
         self.ssl_msg.dribbler = dribbler
         # print(self.ssl_msg)
-        return self.ssl_msg
+        TestSSL.publisher.publish(self.ssl_msg)
+        TestSSL.get_logger().info('Publishing: %s' % self.ssl_msg)
